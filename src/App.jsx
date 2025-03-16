f237eb12c576f62bdadb714c9052bbfd7b443d15
jarnld2@gmail.com <jarnld2@gmail.com>
2025-03-16 13:14:23 -0400

    add @vercel/analytics package and integrate analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Theme } from "@radix-ui/themes";
 import Header from "./Header";
 import JobQueryBuilder from "./JobQueryBuilder";
-import useTheme from "./useTheme"; // Import the custom hook
+import useTheme from "./useTheme";
+import { Analytics } from "@vercel/analytics/react";
+// Import the custom hook
 import "@radix-ui/themes/styles.css";
 
 export default function MyApp() {
@@ -9,6 +11,7 @@ export default function MyApp() {
 
   return (
     <Theme appearance={resolvedTheme}>
+      <Analytics />
       <Header setTheme={setTheme} theme={resolvedTheme} />
       <JobQueryBuilder />
     </Theme>
